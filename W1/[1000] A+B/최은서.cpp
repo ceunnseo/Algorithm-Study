eaa3bd578e7d4a7c5d9acbe310a3b92e28d69763
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2023-07-10 17:19:30 +0900

    Update 최은서.cpp

--- W1/[1000] A+B/최은서.cpp
+++ W1/[1000] A+B/최은서.cpp
@@ -1,9 +1,10 @@
+// 언어 C++, 메모리 2020KB 시간 0ms
 #include <iostream>
 using namespace std;
 
 int main() {
     int a, b; // 더하고자 하는 두 수 변수 선언
-    cin >> a >> b;
+    cin >> a >> b; // 두 수를 입력받음
     cout << a + b; // 합계 출력
     return 0;
 }
